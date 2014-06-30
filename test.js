054693c31169a1e84f6effbe0788c75238dcf85e
Gabriel Wicke <wicke@wikidev.net>
2014-06-30 15:33:28 -0700

    Add a simple test runner, and wire it up to 'npm test'

---
+++ test.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Simple test runner using tests.json data
+
+var tests = require('./tests.json'),
+    model = tests.model,
+    ko = require('./knockoff.js'),
+    assert = require('assert'),
+    options = {
+		globals: {
+			echo: function(i) {
+				return i;
+			},
+			echoJSON: function() {
+				return JSON.stringify(Array.prototype.slice.apply(arguments));
+			}
+		},
+        partials: tests.partials.knockout
+    };
+
+tests.tests.forEach(function(test) {
+    // Test compilation to TAssembly
+    var ta = ko.compile(test.knockout, { toTAssembly: true });
+    assert.equal(JSON.stringify(test.tassembly), JSON.stringify(ta));
+
+    // Test evaluation using TAssembly, implicitly testing the TAssembly
+    // runtime
+    var res = ko.compile(test.knockout, options)(model);
+    assert.equal(JSON.stringify(test.result), JSON.stringify(res));
+});
+
+console.log('\nPASSED', tests.tests.length * 2, 'test cases.');
